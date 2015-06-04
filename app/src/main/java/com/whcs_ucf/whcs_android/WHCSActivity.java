070b247996601f4d958cc2d3640dbde58140f6b1
Jimmy Campbell <jimmypcampbell92@yahoo.com>
2015-06-04 14:20:40 -0400

    GUI and WHCSActivity changes
    Made changes to base station connector gui and also made additions to
    the WHCSActivity class.

--- app/src/main/java/com/whcs_ucf/whcs_android/WHCSActivity.java
+++ app/src/main/java/com/whcs_ucf/whcs_android/WHCSActivity.java
@@ -1,24 +1,43 @@
 package com.whcs_ucf.whcs_android;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by Jimmy on 6/4/2015.
  */
 public class WHCSActivity extends AppCompatActivity {
     private BluetoothAdapter whcsBlueToothAdapter;
+    protected static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         refreshBlueToothAdapter();
+        enableBluetooth();
     }
 
-    private void refreshBlueToothAdapter(){
+    protected void refreshBlueToothAdapter(){
         this.whcsBlueToothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    private void enableBluetooth()
+    {
+        if (!whcsBlueToothAdapter.isEnabled()) {
+            Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+
+            Toast.makeText(this.getApplicationContext(), "Bluetooth turned on",
+                    Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this.getApplicationContext(),"Bluetooth is already on",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }

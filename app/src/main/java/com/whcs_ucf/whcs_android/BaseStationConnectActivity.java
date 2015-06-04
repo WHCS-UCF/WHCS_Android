1bcdd093a84699b81aa6884b7fb488b126631d15
Jimmy Campbell <jimmypcampbell92@yahoo.com>
2015-06-04 11:11:23 -0400

    Colors and theme established

--- app/src/main/java/com/whcs_ucf/whcs_android/BaseStationConnectActivity.java
+++ app/src/main/java/com/whcs_ucf/whcs_android/BaseStationConnectActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class BaseStationConnectActivity extends Activity {
+public class BaseStationConnectActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

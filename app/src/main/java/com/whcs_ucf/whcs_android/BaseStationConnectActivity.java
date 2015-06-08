cebdef459abd27b71a92c7a078a9c7602c6cca2f
Jimmy Campbell <jimmypcampbell92@yahoo.com>
2015-06-08 13:35:46 -0400

    Able to scan for active devices and list paired.

--- app/src/main/java/com/whcs_ucf/whcs_android/BaseStationConnectActivity.java
+++ app/src/main/java/com/whcs_ucf/whcs_android/BaseStationConnectActivity.java
@@ -1,7 +1,12 @@
 package com.whcs_ucf.whcs_android;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,17 +18,26 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.dd.CircularProgressButton;
+
 import java.util.Set;
 
 
 public class BaseStationConnectActivity extends WHCSActivity {
     private ListView pairedList;
     private ListView activeList;
-    private Button pairedbutton;
-    private Button activeButton;
+    private CircularProgressButton pairedbutton;
+    private CircularProgressButton activeButton;
 
     private ArrayAdapter<String> pairedArrayAdapter;
     private ArrayAdapter<String> activeArrayAdapter;
+
+    //some supporting objects are needed to discover active BlueTooth Devices.
+    //We need to start an asynchronous device finder and then register a
+    //Broadcast receiver to our application that way when the device finder finishes
+    //Our application will catch the fact that it is complete.
+    //An Intent filter is used to catch the
+    private BroadcastReceiver activeDeviceReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,33 +86,66 @@ public class BaseStationConnectActivity extends WHCSActivity {
         activeList.setAdapter(activeArrayAdapter);
 
         //Establishes the references to the buttons in the basestation connection activity.
-        this.pairedbutton = (Button)this.findViewById(R.id.pairedButton);
-        this.activeButton = (Button)this.findViewById(R.id.activeButton);
+        this.pairedbutton = (CircularProgressButton)this.findViewById(R.id.pairedButton);
+        this.activeButton = (CircularProgressButton)this.findViewById(R.id.activeButton);
+
+        //We want activeButton to be showing when it is scanning for devices.
+        this.activeButton.setIndeterminateProgressMode(true);
     }
 
     private void setupButtonListeners() {
         pairedbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pairedArrayAdapter.clear();
-                for(int i=0; i<5; i++){
-                    pairedArrayAdapter.add("Device number #" + i);
-                }
+                listPairedDevices();
             }
         });
 
         activeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activeArrayAdapter.clear();
-                for(int i=0; i<5; i++){
-                    activeArrayAdapter.add("Device number #" + i);
-                }
+                listActiveDevices();
             }
         });
+
+        // Create a BroadcastReceiver for ACTION_FOUND
+        activeDeviceReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    activeArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+                else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                    //We want the active device button to stop the refeshing effect.
+                    activeButton.setProgress(0);
+                }
+            }
+        };
+
+        // Register the BroadcastReceiver
+        //This must be called after activeDeviceReceiver is initialized with the BlueTooth Discovery handler
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(activeDeviceReceiver, filter); // Don't forget to unregister during onDestroy
+
+        //Register to receive when BlueTooth discovery stops.
+        IntentFilter discoveryStopFilter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(activeDeviceReceiver, discoveryStopFilter);
     }
 
-    private void ListPairedDevices() {
+    private void setupListListeners() {
+
+    }
+
+    private void listPairedDevices() {
+
+        if(DebugFlags.RUNNING_ON_VM){
+            listDevicesForVM(pairedArrayAdapter);
+            return;
+        }
 
         // get paired devices
 
@@ -110,7 +157,41 @@ public class BaseStationConnectActivity extends WHCSActivity {
         // put it's one to the adapter
         for(BluetoothDevice device : pairedDevices)
             pairedArrayAdapter.add(device.getName()+ "\n" + device.getAddress());
+    }
 
-        Toast.makeText(this.getApplicationContext(), "Show Paired Devices", Toast.LENGTH_SHORT).show();
+    private void listActiveDevices() {
+
+        if(DebugFlags.RUNNING_ON_VM){
+            listDevicesForVM(activeArrayAdapter);
+            return;
+        }
+        if(whcsBlueToothAdapter.isDiscovering()) {
+            //We're already discovering. We need to wait for that to finish.
+            return;
+        }
+        //Clear the arrayAdapter for a refresh
+        activeArrayAdapter.clear();
+
+
+        //Begin discovery but notify through text if we weren't able to for some reason.
+        if(!whcsBlueToothAdapter.startDiscovery()) {
+            Toast.makeText(this.getApplicationContext(), "BlueTooth Discovery couldn't start.", Toast.LENGTH_LONG);
+            return;
+        }
+        //Set progress between 1-99 to get the refreshing look.
+        this.activeButton.setProgress(50);
+
+    }
+
+    private void listDevicesForVM(ArrayAdapter<String> aa) {
+        aa.clear();
+        for(int i=0; i<5; i++) {
+            aa.add("Fake Device number #" + i);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(activeDeviceReceiver);
     }
 }
--- app/src/main/java/com/whcs_ucf/whcs_android/DebugFlags.java
+++ app/src/main/java/com/whcs_ucf/whcs_android/DebugFlags.java
@@ -4,5 +4,5 @@ package com.whcs_ucf.whcs_android;
  * Created by Jimmy on 6/8/2015.
  */
 public class DebugFlags {
-    public static final boolean RUNNING_ON_VM = true;
+    public static final boolean RUNNING_ON_VM = false;
 }
